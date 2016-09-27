7d4f331db74a04a4e4e136e0ddbc9e247959cc5a
Dongen, Thomas T.G.T.A. van <tgta.vandongen@student.fontys.nl>
2016-09-27 11:20:15 +0200

    Aanmaken project

---
+++ UnittestenTime/test/fontys/time/TimeTest.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fontys.time;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Thomas
+ */
+public class TimeTest {
+    
+    private Time time;
+    
+    public TimeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        time = new Time(2016, 9, 20, 11, 25);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getDayInWeek method, of class Time.
+     */
+    @Test
+    public void testGetDayInWeek() {
+        System.out.println("getDayInWeek");
+        DayInWeek expResult = DayInWeek.TUE;
+        DayInWeek result = time.getDayInWeek();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getYear method, of class Time.
+     */
+    @Test
+    public void testGetYear() {
+        System.out.println("getYear");
+        int expResult = 2016;
+        int result = time.getYear();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getMonth method, of class Time.
+     */
+    @Test
+    public void testGetMonth() {
+        System.out.println("getMonth");
+        int expResult = 9;
+        int result = time.getMonth();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getDay method, of class Time.
+     */
+    @Test
+    public void testGetDay() {
+        System.out.println("getDay");
+        int expResult = 20;
+        int result = time.getDay();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getHours method, of class Time.
+     */
+    @Test
+    public void testGetHours() {
+        System.out.println("getHours");
+        int expResult = 11;
+        int result = time.getHours();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getMinutes method, of class Time.
+     */
+    @Test
+    public void testGetMinutes() {
+        System.out.println("getMinutes");
+        int expResult = 25;
+        int result = time.getMinutes();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of plus method, of class Time.
+     */
+    @Test
+    public void testPlus() {
+        System.out.println("plus");
+        int minutes = 20;
+        int expResult = 45;
+        ITime result = time.plus(minutes);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of compareTo method, of class Time.
+     */
+    @Test
+    public void testCompareTo() {
+        System.out.println("compareTo");
+        ITime t = time;
+        int expResult = 0;
+        int result = time.compareTo(t);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of difference method, of class Time.
+     */
+    @Test
+    public void testDifference() {
+        System.out.println("difference");
+        ITime t = time;
+        int expResult = 0;
+        int result = time.difference(t);
+        assertEquals(expResult, result);
+    
+    }
+    
+}
